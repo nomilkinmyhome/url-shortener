47feafb518ff0073f14c31d21caa9c5a3b8571e0
Polina Beskorovainaia <polina.beskorovainaia@mysky.com>
2023-10-11 12:24:57 +0300

    fix front

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css';
+
+import { useState } from 'react';
+
+function App() {
+  const [ urlInput, setUrlInput ] = useState("");
+  const [ shortedURL, setShortedURL ] = useState("");
+
+  function submitHandler(event) {
+      fetch(
+          'http://localhost:8000/?url=' + urlInput,
+          {method: 'GET'}
+      ).then(r => {
+          return r.json();
+      }).then((data) => {
+          setShortedURL(data.url);
+      });
+  }
+
+  return (
+    <div className="App">
+        <h2>URL Shortener</h2>
+        <div className="container">
+            <form>
+                <input type="text" placeholder="Enter URL" value={ urlInput } onChange={ setUrlInput.target.value } />
+                <button onClick={ submitHandler }>Short!</button>
+            </form>
+            {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
+            { shortedURL ? <a href={shortedURL}></a> : "" }
+        </div>
+    </div>
+  );
+}
+
+export default App;
